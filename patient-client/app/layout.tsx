30b3a03d76c5d49f0bed35038423e1d58a909fd0
Anmol Garg <anmolgarg271005@gmail.com>
2025-09-07 13:33:04 +0530

    "solve client issue"

--- patient-client/app/layout.tsx
+++ patient-client/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { AppProvider } from './context/AppContext';
+import { LanguageProvider } from './providers/LanguageProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +18,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className=" flex items-center justify-center">
+        <LanguageProvider>
+          <AppProvider>
+            <div className="w-full max-w-md bg-white dark:bg-gray-800 shadow-xl min-h-screen">
+              {children}
+            </div>
+          </AppProvider>
+        </LanguageProvider>
+      </body>
     </html>
   );
 }
